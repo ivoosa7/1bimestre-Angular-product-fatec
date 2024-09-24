2938fb111c87bae442987244e8c34b79bd3e68d4
Ivo Araujo <ivo.souza-23@outlook.com>
2024-09-24 10:07:42 -0300

    Sixth Commit class half 24/09

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit{
+  constructor(private router: Router,
+              private activeRouter: ActivatedRoute,
+              private service: ProductService)
+              {
+  }
 
+  ngOnInit() {
+    const id = Number(this.activeRouter.snapshot.paramMap.get("id"));
+    this.loadProduct(id);
+  }
+
+  loadProduct(id:number){
+    this.service.getProductById(id).subscribe({
+      next: data => alert(data.name)
+    });
+  }
 }
